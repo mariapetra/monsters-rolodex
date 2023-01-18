411faa309be18b35e666e88a94f8dd1cc042094b
Mara Petra <62734302+mariapetra@users.noreply.github.com>
2023-01-18 17:24:36 +0800

    test branch

--- src/App.js
+++ src/App.js
@@ -9,14 +9,20 @@ class App extends Component {
     this.state = {
       monsters: [
         {
-          name: 'Maria'
+          name: 'Maria',
+          id: '876sugdfugsdf'
         },
         {
-          name: 'Laura'
+          name: 'Laura',
+          id: '76khfgkudfhkgh'
+          //testing my new branch
+
         },
 
         {
-          name: 'Louise'
+          name: 'Louise',
+          id: 'kjhkd6r98erihd98'
+
         },
       ]
 
@@ -30,7 +36,16 @@ class App extends Component {
         //what is map? map is an array method 
         //use a function to itterate through your array and create a new array based on your existing arrays
         this.state.monsters.map((monster) => {
-          return <h1>{monster.name}</h1>;
+          //most projects will have an id value - as this is unique / you will get an error that you should provide a unique key
+          //this is what we are using in our key value
+          //why - react and re rendering - the key value is used by react to make it more optimised to update and re render components on your page
+          //eg we have 4 h1s - react updates this directky
+          //how are these h1s differentiate? - by using  the key value
+          // react will use this to know who to update
+          return <div>
+
+            <h1 key={monster.id}>{monster.name}</h1>
+          </div>
         }
         )
       }
